a1a2174934dfbae1b70f3eee623c0830ffe0b983
rramires <ricardo@flexbr.com>
2026-06-15 09:45:38 -0300

    feat: update typeScript and vite configuration for path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
+	server: {
 		port: 3001,
 	},
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 })
